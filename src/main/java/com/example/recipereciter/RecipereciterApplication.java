5ef2fd7527ceee13eff86ec28df37e5303526b79
kansenchu <kansen.chu@g.softbank.co.jp>
2019-08-12 00:07:00 +0900

    initial commit

---
+++ src/main/java/com/example/recipereciter/RecipereciterApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipereciter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipereciterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipereciterApplication.class, args);
+	}
+
+}
