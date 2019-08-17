e11cc23bc72ec43005ca29b1601b17a1643c1722
kansenchu <kansen.chu@g.softbank.co.jp>
2019-08-17 15:25:31 +0900

    RecipeServiceドキュメンテーション追加

--- src/main/java/com/example/recipereciter/service/RecipeService.java
+++ src/main/java/com/example/recipereciter/service/RecipeService.java
@@ -1,13 +1,29 @@
 package com.example.recipereciter.service;
 
 import com.example.recipereciter.dto.Recipe;
+import com.example.recipereciter.exception.NoRecipeFoundException;
 
 import java.util.List;
 
+/**
+ * レシピのサービスの契約を示すインタフェース。
+ */
 public interface RecipeService {
+    /**
+     * 全レシピ取得。
+     *
+     * @return 全レシピのリスト
+     */
     List<Recipe> getAllRecipes();
 
-    Recipe getRecipe(int id);
+    /**
+     * 指定したIDのレシピを返す。
+     *
+     * @param id 欲しいレシピ
+     * @return 指定したレシピ
+     * @throws NoRecipeFoundException レシピ見つかれない時
+     */
+    Recipe getRecipe(int id) throws NoRecipeFoundException;
 
     Recipe addRecipe(Recipe newRecipe);
 
---
+++ src/test/java/com/example/recipereciter/service/BasicRecipeServiceTest.java
@@ -0,0 +1,7 @@
+package com.example.recipereciter.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicRecipeServiceTest {
+
+}
